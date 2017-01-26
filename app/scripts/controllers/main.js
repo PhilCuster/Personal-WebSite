4f82065c85bcbb625d9774ff408b8c28c468ffa9
Phil Custer <custer55@gmail.com>
2017-01-25 21:51:23 -0500

    Started setting up main page.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the untitledApp
  */
 angular.module('untitledApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['$scope', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.age = calculateAge();
+
+    function calculateAge() {
+      var birthDate = new Date("4/22/1994");
+      var today = new Date();
+      return Math.floor((today.getTime() - birthDate.getTime()) / (1000 * 3600 * 24 * 365));
+    }
+  }]);
